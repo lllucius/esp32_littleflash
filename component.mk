f4f777d11a6ebc3343fc5d2c35c18e10eb70a51e
Leland Lucius <github@homerow.net>
2019-03-14 12:59:58 -0500

    Allow use of main tree to be used as component

---
+++ component.mk
@@ -0,0 +1,8 @@
+#
+# Component Makefile
+#
+
+COMPONENT_SRCDIRS = components/$(COMPONENT_NAME)
+COMPONENT_ADD_INCLUDEDIRS = components/$(COMPONENT_NAME)/include \
+							components/$(COMPONENT_NAME)/littlefs
+
